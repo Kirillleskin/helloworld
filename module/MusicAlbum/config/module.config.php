e92ffac07a7451315bec5ec76610b27c9a0428b5
Kirill Leskin <kirillleskin@gmail.com>
2017-03-01 04:36:29 +0300

    doctrine init

--- module/MusicAlbum/config/module.config.php
+++ module/MusicAlbum/config/module.config.php
@@ -12,6 +12,28 @@ use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
+    'doctrine' => [
+        'driver' => [
+            // defines an annotation driver with two paths, and names it `my_annotation_driver`
+            'music_album_db_driver' => [
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => [
+                    __DIR__ . '/../src/MusicAlbum/Entity'
+                ],
+            ],
+
+            // default metadata driver, aggregates all other drivers into a single one.
+            // Override `orm_default` only if you know what you're doing
+            'orm_default' => [
+                'drivers' => [
+                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
+                    'MusicAlbum\Entity' => 'music_album_db_driver'
+                ]
+            ]
+        ]
+    ],
+
     'router' => [
         'routes' => [
 
