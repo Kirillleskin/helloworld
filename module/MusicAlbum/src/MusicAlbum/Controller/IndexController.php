f87f0b4955b39879e08079b7c63098d670171ad4
Kirill Leskin <kirillleskin@gmail.com>
2017-03-01 22:11:04 +0300

    doctrine entity manager

--- module/MusicAlbum/src/MusicAlbum/Controller/IndexController.php
+++ module/MusicAlbum/src/MusicAlbum/Controller/IndexController.php
@@ -9,11 +9,32 @@ namespace MusicAlbum\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use MusicAlbum\Entity\Album;
+use Doctrine;
 
 class IndexController extends AbstractActionController
 {
+    /**
+     * Entity manager.
+     * @var Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    // Constructor method is used to inject dependencies to the controller.
+    public function __construct($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function indexAction()
     {
+        $album = new Album();
+        $album->setName("Album name");
+        $album->setDescription("Album description");
+
+        $this->entityManager->persist($album);
+        $this->entityManager->flush();
+
         return new ViewModel();
     }
 }
---
+++ module/MusicAlbum/src/MusicAlbum/Entity/Album.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace MusicAlbum\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Album
+ *
+ * @ORM\Table(name="album")
+ * @ORM\Entity
+ */
+class Album
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=45, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=45, nullable=true)
+     */
+    private $description;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Album
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Album
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
